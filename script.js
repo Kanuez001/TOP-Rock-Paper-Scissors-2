2d355f14abbb3d6474b04bedb6722efe4cfceb81
Kanu Ezindu <kanuezindu@gmail.com>
2022-08-17 07:12:45 +0100

    Get computer's random choice

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let playerInput = prompt("Enter your option");
 let playerSelection = playerInput.toUpperCase();
+
+const options = ["ROCK", "PAPER", "SCISSORS"];
+function getComputerChoice(){
+    const randomOption = options[Math.floor(Math.random() * options.length)]
+    return randomOption;
+}
+let computerSelection = getComputerChoice();
