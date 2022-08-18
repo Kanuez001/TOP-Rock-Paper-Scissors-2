f0e78ac47014c093bf5f8953d610eedf1c41e421
Kanu Ezindu <kanuezindu@gmail.com>
2022-08-18 08:47:59 +0100

    Add the game() function to script.js
    This function loops the playRound() function, keeps the score and reports the winner or loser at the end.

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-let playerInput = prompt("Enter your option");
-let playerSelection = playerInput.toUpperCase();
-
-const options = ["ROCK", "PAPER", "SCISSORS"];
-function getComputerChoice(){
-    const randomOption = options[Math.floor(Math.random() * options.length)]
-    return randomOption;
-}
-let computerSelection = getComputerChoice();
-console.log(computerSelection)
-
-function playRound(playerSelection, computerSelection){
+function game(){
     const tie = "It's a tie. Try again!"
     const R1 = "You lose! Paper swallows Rock"
     const R2 = "You win! Rock smashes Scissors"
@@ -18,37 +7,73 @@ function playRound(playerSelection, computerSelection){
     const S1 = "You lose! Rock smashes Scissors"
     const S2 = "You win! Scissors shreds Paper"
     const wrongInput = "Enter a valid option!"
+    let playerScore = 0;
+    let computerScore = 0;
 
-    if (playerSelection === "ROCK" && computerSelection === "ROCK"){
-        return tie;
+    for (i = 0; i < 5; i++){
+        let playerInput = prompt("Enter your option");
+        let playerSelection = playerInput.toUpperCase();
+
+        const options = ["ROCK", "PAPER", "SCISSORS"];
+        function getComputerChoice(){
+            const randomOption = options[Math.floor(Math.random() * options.length)]
+            return randomOption;
+        }
+        let computerSelection = getComputerChoice();
+        console.log(computerSelection)
+
+        function playRound(playerSelection, computerSelection){
+            if (playerSelection === "ROCK" && computerSelection === "ROCK"){
+                return tie;
+            }
+            else if (playerSelection === "ROCK" && computerSelection === "PAPER"){
+                computerScore++;
+                return R1;
+            }
+            else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
+                playerScore++;
+                return R2;
+            }
+            else if (playerSelection === "PAPER" && computerSelection === "ROCK"){
+                playerScore++;
+                return P1;
+            }
+            else if (playerSelection === "PAPER" && computerSelection === "PAPER"){
+                return tie;
+            }
+            else if (playerSelection === "PAPER" && computerSelection === "SCISSORS"){
+                computerScore++;
+                return P2;
+            }
+            else if (playerSelection === "SCISSORS" && computerSelection === "ROCK"){
+                computerScore++;
+                return S1;
+            }
+            else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
+                playerScore++;
+                return S2;
+            }
+            else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS"){
+                return tie;
+            }
+            else{
+                return wrongInput;
+            }
+        }
+        console.log(playRound(playerSelection, computerSelection))
     }
-    else if (playerSelection === "ROCK" && computerSelection === "PAPER"){
-        return R1;
+    let totalScore = `FINAL RESULT 
+    Player: ${playerScore} | Computer: ${computerScore}`
+    console.log(totalScore);
+
+    if (playerScore > computerScore) {
+        console.log("Congrats! You win");
     }
-    else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
-        return R2;
+    else if (computerScore > playerScore) {
+        console.log("You lose! Try again");
     }
-    else if (playerSelection === "PAPER" && computerSelection === "ROCK"){
-        return P1;
-    }
-    else if (playerSelection === "PAPER" && computerSelection === "PAPER"){
-        return tie;
-    }
-    else if (playerSelection === "PAPER" && computerSelection === "SCISSORS"){
-        return P2;
-    }
-    else if (playerSelection === "SCISSORS" && computerSelection === "ROCK"){
-        return S1;
-    }
-    else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
-        return S2;
-    }
-    else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS"){
-        return tie;
-    }
-    else{
-        return wrongInput;
+    else {
+        console.log("It's a tie! Play again");
     }
 }
-
-console.log(playRound(playerSelection, computerSelection))
+console.log(game());
